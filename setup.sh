5efed4295a6316d223c1ed102fb54fb012481087
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2019-09-03 14:23:46 -0400

    New script.

--- setup.sh
+++ setup.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+set -e
 
+brew cask
+brew install dropbox
+brew install vim
+brew install mackup
+mackup restore
+
+mkdir -p ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +BundleInstall +qall
+cd ~/.vim/bundle/Command-T/ruby/command-t/ext/command-t
+PATH="/usr/local/opt/ruby/bin:$PATH" ruby extconf.rb
+make
+
+brew cask install google-chrome
